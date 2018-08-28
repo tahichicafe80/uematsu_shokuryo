c7ae6a4b09f9161a40b2f3eb2dc7579ffd757e6b
Shinsuke Uematsu <tahichicafe80@gmail.com>
2018-08-28 16:50:55 +0900

    su
    su

---
+++ js/main_factory.js
@@ -0,0 +1,67 @@
+// DOM Content Loaded
+$(function(){
+
+
+    console.log('読み込んだよ');
+
+    // スライドショー
+    $('.slider').slick({
+        arrows:false, /* 次への矢印 */
+        autoplay:true, /* 自動更新 */
+        //adaptiveHeight: true, /* 高さの自動調整 */
+        dots: true, /* 小さい丸をだすかどうか */
+        autoplaySpeed : 2000, /* 切り替えの速さ */
+        fade:true,
+        pauseOnHover: false /* 最後の行はカンマいらない */
+    });
+
+    //   // モーダル
+    //   var modalImg = ['img/works01.png','img/works02.png','img/works03.png'];
+    //   // プリロード
+    //   for(var i=0; i<modalImg.length; i++){
+    //     $('<img>').attr('src' ,modalImg[i]);
+    //   }
+    //
+    //   $('#works').find('section').on('click', function(){
+    //
+    //       // sectionのなんばんめを押したか
+    //       var index = $('#works').find('section').index(this);
+    //       var modal = $('<div class="modal"><img src="'+ modalImg[index] +'" alt=""></div>')
+    //       $('#overlay').html(modal);
+    //
+    //       console.log(index);
+    //       $('#overlay').css('display','block').addClass('fade-in')
+    //       $('#overlay').on('click',function(){
+    //          $(this).css('display','none').removeClass('fade-in');
+    //       });
+    //   });
+
+
+    $(window).on('scroll',function(){
+        // 上からのスクロール値
+        var dy = $(this).scrollTop();
+        // console.log(dy);
+
+// index.htmlのjs ---------------------
+        // #works .containerの高さを取得
+        var titleTop = $('.title').offset().top;
+        // console.log(worksContainerTop);
+
+        // スクロールが#works .containerのY座標を超えらたら
+        if(dy >= titleTop - $(window).height()){
+            // sectionに.fade-inのクラスを追加する
+            $('.title').addClass('fade-in');
+        }
+
+        // スクロールが#works .containerのY座標を超えらたら
+        if(dy >= titleTop - $(window).height()){
+            // sectionに.fade-inのクラスを追加する
+            $('#factory').addClass('fade-in');
+        }
+
+
+    });
+
+
+
+});
